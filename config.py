8e194aa0005ffafbc2e58766f6e665c685e9be0e
KingVishal703 <vishal21wankhede@gmail.com>
2025-04-19 21:36:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5654093580"))
 #Port
 PORT = os.environ.get("PORT", "8030")
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://timed29716:jzE1SqRNktFycmVo@cluster0.kco6t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #Time in seconds for message delete, put 0 to never delete
