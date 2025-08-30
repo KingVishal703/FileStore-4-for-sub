268c4f349e079e22111cc2906810426c4f12ad4e
KingVishal703 <vishal21wankhede@gmail.com>
2025-08-31 00:35:04 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,49 +1,32 @@
-#(©)Codexbotz
-
-import asyncio
+from io import BytesIO
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import FloodWait
-
 from bot import Bot
 from config import ADMINS, AUTO_POST_CHANNEL, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start', 'id','users','broadcast','batch','genlink','stats']))
+@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','id','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote=True)
-
-    # Copy video to db_channel for link generation
-    try:
-        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
-    except Exception as e:
-        print(e)
-        await reply_text.edit_text("Something went wrong..!")
-        return
-
-    # Generate unique link
-    converted_id = post_message.id * abs(client.db_channel.id)
+    
+    # Generate link for video
+    converted_id = message.id * 123456  # simple unique multiplier
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-
-    reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]]
-    )
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
 
-    # Send thumbnail image to AUTO_POST_CHANNEL
     try:
-        # Check if message has video with thumbnail
+        # Thumbnail handling
         if message.video and message.video.thumbs:
-            thumb = message.video.thumbs[0].file_id
+            thumb_file = await client.download_media(message.video.thumbs[0].file_id, in_memory=True)
+            thumb_bytes = BytesIO(thumb_file)
+            thumb_bytes.name = "thumbnail.jpg"
             await client.send_photo(
                 chat_id=AUTO_POST_CHANNEL,
-                photo=thumb,
+                photo=thumb_bytes,
                 caption=f"🎬 <b>New Video Uploaded!</b>\n\n🔗 <b>Link:</b> {link}",
                 reply_markup=reply_markup
             )
@@ -51,16 +34,9 @@ async def channel_post(client: Client, message: Message):
             # Default image if no thumbnail
             await client.send_photo(
                 chat_id=AUTO_POST_CHANNEL,
-                photo="https://telegra.ph/file/0c6f1a29e9d92b7d8e8a1.jpg",  # Replace with your default image
+                photo="https://telegra.ph/file/0c6f1a29e9d92b7d8e8a1.jpg",
                 caption=f"🎬 <b>New Video Uploaded!</b>\n\n🔗 <b>Link:</b> {link}",
                 reply_markup=reply_markup
             )
     except Exception as e:
         print("Auto post failed:", e)
-
-    # Edit reply markup on db_channel post if buttons enabled
-    if not DISABLE_CHANNEL_BUTTON:
-        try:
-            await post_message.edit_reply_markup(reply_markup)
-        except Exception as e:
-            pass
