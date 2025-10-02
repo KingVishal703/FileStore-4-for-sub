6d000d0c56d5da4e1e82e68bc0d6404bb50f6212
KingVishal703 <vishal21wankhede@gmail.com>
2025-10-03 01:46:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,8 +61,8 @@ TOKEN = True if os.environ.get('TOKEN', "True") == "True" else False  #For Enabl
 
 #TOKEN = False if os.environ.get('TOKEN', "FALSE") == 'FALSE' else False # For disable Token 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "linkcents.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "dcc4e31e2f3fd1cb80fbefb817bbe26cc46d7708")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/hentai_Hanime_Update_Channel/72")
